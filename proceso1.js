700dee930cc1131000c24e9f4f646a6163f93ed3
Christopher Bruno Carrillo Pulido <98793732+ChBrus@users.noreply.github.com>
2022-07-11 12:40:58 -0500

    Añadiendo archivos de pupleaños
    Pal cumple de Vianca :D

---
+++ proceso1.js
@@ -0,0 +1,13 @@
+//var webPage = document.getElementById("divChanging");
+var rm = document.getElementById("desaparecer");
+
+rm.addEventListener("click", removeFunction);
+
+function removeFunction() {
+    //webPage.textContent = "<div></div>";
+
+    document.write("<h1>Hola</h1>");
+
+    //createNewPage();
+}
+
